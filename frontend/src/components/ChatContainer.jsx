f00c849b4732ab031c45ea225ff8ac4a4ef4df75
AbhishekGupta7920 <abgupta12558@gmail.com>
2025-03-10 22:45:42 +0530

    frontend code

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,84 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
+
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
+
+const ChatContainer = () => {
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <div className="flex-1 overflow-y-auto">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          >
+            <div className="chat-bubble relative  my-1 px-3">
+              {/* Text/Image Content */}
+              <div className="px-2">
+                {message.image && (
+                  <img
+                    src={message.image}
+                    alt="Attachment"
+                    className="sm:max-w-[200px] rounded-md mb-3"
+                  />
+                )}
+                {message.text && <p className=" mb-2">{message.text}</p>}
+              </div>
+
+              {/* Timestamp at Bottom-Right */}
+              <time className="absolute bottom-1 right-2 text-xs opacity-50 pt-2">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+          </div>
+
+
+        ))}
+      </div>
+
+      <MessageInput />
+    </div>
+  );
+};
+export default ChatContainer;
